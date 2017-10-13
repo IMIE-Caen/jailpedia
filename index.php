0c03a2021c362c0c06446e301e06a1340560831b
Clément Alexandre <clmntlxndr@gmail.com>
2017-10-13 14:31:33 +0200

    Updated rooter with regex

--- index.php
+++ index.php
@@ -1,28 +1,33 @@
 <?php
 ini_set('display_errors', 'On');
-error_log("Bienvenue sur mon router");
-
 include_once("./my_http.php");
+include_once("./controllers/HomeController.php");
+include_once("./controllers/ArticlesController.php");
 
 $request = new MyHttp();
+$preg_match_results = [];
+
 
 if($request->pathInfo() == "/"){
 
-  ob_start();
-  include("./views/index.html.php");
-  $page_content = ob_get_clean();
+  $controller = new HomeController();
+  $controller->render();
 
-  //$page_content = "COUCOU";
-
-  include("./views/layout.html.php");
 }
-else {
-  if($request->pathInfo() == "/pouet"){
-    echo "Page pouet";
-  }
-  else{
-    header("HTTP/1.0 404 Not Found");
-    echo file_get_contents("./views/404.html.php");
-    echo "La page n'existe pas";
-  }
+
+// /articles/12345
+elseif(
+    preg_match('/^\/articles\/(\d+)\/?$/',
+                $request->pathInfo(),
+                $preg_match_results) ){
+
+    $id = $preg_match_results[1];
+    $controller = new ArticlesController();
+    $controller->show($id);
+
+}
+else{
+  header("HTTP/1.0 404 Not Found");
+  echo file_get_contents("./views/404.html.php");
+  echo "La page n'existe pas";
 }
