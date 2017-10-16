999c2e35ca287e50e979975fdb031ffe88a9ef71
Valentin GALLIEN <v.gallien@yahoo.fr>
2017-10-16 14:33:06 +0200

    Amélioration du design général

--- views/header.html.php
+++ views/header.html.php
@@ -1,25 +1,23 @@
-<header>
-    <div class="container">
-        <div class="row">
-            <a class="logo col-2" href="/" title="Jailpedia">
-                <img class="img-fluid" src="/images/logo.png" alt="Logo Jailpedia" title="Jailpedia" />
-            </a>
-            <form class="col-6 form-group row">
-                <div class="form-row align-items-center">
-                    <div class="col-auto">
-                        <input class="form-control mr-sm-2" type="text" placeholder="Rechercher" aria-label="Rechercher">
-                    </div>
-                    <div class="col-auto">
-                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Rechercher</button>
-                    </div>
-                </div>
-            </form>
-          <div class="headline col-4">
-            <span class="headline-l1">
-              J<span class="jaune">a</span>i<span class="jaune">l</span>p<span class="jaune">e</span>d<span class="jaune">i</span>a
-            </span>
-            <span class="headline-l2">Préparez votre évasion</span>
-          </div>
+<header class="container">
+  <div class="row">
+    <a class="logo col-2" href="/" title="Jailpedia">
+      <img class="img-fluid" src="/images/logo.png" alt="Logo Jailpedia" title="Jailpedia" />
+    </a>
+    <form class="col-6 form-group row">
+      <div class="form-row align-items-center">
+        <div class="col-auto">
+          <input class="form-control mr-sm-2" type="text" placeholder="Rechercher" aria-label="Rechercher">
         </div>
+        <div class="col-auto">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Rechercher</button>
+        </div>
+      </div>
+    </form>
+    <div class="headline col-4">
+      <span class="headline-l1">
+        <span>Jail</span><span>pedia</span>
+      </span>
+      <span class="headline-l2">Préparez votre évasion</span>
     </div>
+  </div>
 </header>
