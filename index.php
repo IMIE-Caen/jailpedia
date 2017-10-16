6b54d87b65feebe153678261bae1674f2719bd75
Valentin GALLIEN <v.gallien@yahoo.fr>
2017-10-16 11:02:11 +0200

    Modif du menu

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 ini_set('display_errors', 'On');
 
 include_once("./PDO.php");
@@ -11,7 +12,7 @@ $articles_list = $stmt->fetchAll(PDO::FETCH_ASSOC);
 $stmt->closeCursor();
 //Parcourir la liste des membres
 foreach ($articles_list as $row) {
-    echo 'Identifiant'.$row['ME_ID'].'Nom'.$row['ME_NAME']."<br />";
+//    echo 'Identifiant'.$row['ME_ID'].'Nom'.$row['ME_NAME']."<br />";
 }
 echo "<br />";
 $sql = 'SELECT * FROM USERS';
@@ -22,126 +23,93 @@ $users_list = $stmt->fetchAll(PDO::FETCH_ASSOC);
 $stmt->closeCursor();
 //Parcourir la liste des membres
 foreach ($users_list as $row) {
-    echo 'Identifiant'.$row['ME_ID']. 'Prenom '.$row['ME_NAME'].' Nom '.$row['ME_LASTNAME']."<br />";
+//    echo 'Identifiant'.$row['ME_ID']. 'Prenom '.$row['ME_NAME'].' Nom '.$row['ME_LASTNAME']."<br />";
 }
 
 function __autoload($className) {
   if (file_exists($className . '.php')) {
     require_once $className . '.php';
     return true;
-  }
-  else if (file_exists("./controllers/".$className . '.php')){
-    require_once "./controllers/".$className . '.php';
+  } else if (file_exists("./controllers/" . $className . '.php')) {
+    require_once "./controllers/" . $className . '.php';
     return true;
   }
-  return false; 
-
-
-
+  return false;
 }
 
 $request = new MyHttp();
 $preg_match_results = [];
 
 
-if($request->pathInfo() == "/"){
+if ($request->pathInfo() == "/") {
 
   $controller = new HomeController();
   $controller->render();
-
 }
 
 
-if ($request->method()== "GET"){
-    // /articles/12345
-    if(
-      preg_match('/^\/articles\/(\d+)\/?$/',
-        $request->pathInfo(),
-        $preg_match_results) ){
- 
-      $id = $preg_match_results[1];
-      $controller = new ArticlesController();
-      $controller->show($id);
+if ($request->method() == "GET") {
+  // /articles/12345
+  if (
+          preg_match('/^\/articles\/(\d+)\/?$/', $request->pathInfo(), $preg_match_results)) {
 
-    }
-
-    // /users/123
-    elseif (preg_match('/^\/users\/(\w+)\/?$/',
-      $request->pathInfo(),
-      $preg_match_results)) {
-
-      $id = $preg_match_results[1]; 
-    $controller = new UsersController(); 
-    $controller->show($id); 
+    $id = $preg_match_results[1];
+    $controller = new ArticlesController();
+    $controller->show($id);
   }
 
-      // /tags/{id}
-    elseif (preg_match('/^\/tags\/(\d+)\/?$/',
-      $request->pathInfo(),
-      $preg_match_results)) {
-      $id = $preg_match_results[1]; 
-    $controller = new TagsController(); 
-    $controller->show($id);  
-    }
+  // /users/123
+  elseif (preg_match('/^\/users\/(\w+)\/?$/', $request->pathInfo(), $preg_match_results)) {
 
-} 
+    $id = $preg_match_results[1];
+    $controller = new UsersController();
+    $controller->show($id);
+  }
+
+  // /tags/{id}
+  elseif (preg_match('/^\/tags\/(\d+)\/?$/', $request->pathInfo(), $preg_match_results)) {
+    $id = $preg_match_results[1];
+    $controller = new TagsController();
+    $controller->show($id);
+  }
+}
 // si méthode HTTP est POST 
-elseif($request->method()== "GET"){
+elseif ($request->method() == "GET") {
 
   // recuperer les articles 
   // /articles
-  if(
-    preg_match('/^\/articles\/?$/',
-      $request->pathInfo())){
-   $controller = new ArticlesController();
-   $controller->showAllArticles();
+  if (
+          preg_match('/^\/articles\/?$/', $request->pathInfo())) {
+    $controller = new ArticlesController();
+    $controller->showAllArticles();
   }
 
-   //ajout article 
+  //ajout article 
   // /articles/create
-   else if(
-    preg_match('/^\/articles\/create\/?$/',
-      $request->pathInfo())){
-     $controller = new ArticlesController();
+  else if (
+          preg_match('/^\/articles\/create\/?$/', $request->pathInfo())) {
+    $controller = new ArticlesController();
     $controller->save();
-
   }
 
 
   // /tags 
-   else if (preg_match('/^\/tags\/?$/',
-    $request->pathInfo())) {
-    $controller = new TagsController(); 
-  $controller->showAllTags(); 
-}
+  else if (preg_match('/^\/tags\/?$/', $request->pathInfo())) {
+    $controller = new TagsController();
+    $controller->showAllTags();
+  }
+} elseif ($request->method() == "PATCH") {
 
-
-
-
-}
-
-elseif($request->method()== "PATCH"){
-  
   // modifier un article 
   // /articles/update 
-  if(
-    preg_match('/^\/articles\/edit\/(\d+)\/?$/',
-      $request->pathInfo(),
-      $preg_match_results) ){
+  if (
+          preg_match('/^\/articles\/edit\/(\d+)\/?$/', $request->pathInfo(), $preg_match_results)) {
     $id = $preg_match_results[1];
     $controller = new ArticlesController();
     $controller->edit($id);
-
   }
-
-
-
-} 
-
-
-
-else{
-   var_dump("toto");
+} else {
+  var_dump("toto");
   header("HTTP/1.0 404 Not Found");
   echo file_get_contents("./views/404.html.php");
   echo "La page n'existe pas";
--- views/menu.html.php
+++ views/menu.html.php
@@ -1,7 +1,10 @@
 <nav class="navbar navbar-expand-lg bg-light">
     <ul class="navbar-nav">
-        <li>
-            <a href="#" title="Créer un compte">Créer un compte</a>
+        <li class="nav-item">
+            <a class="nav-link" href="#" title="Se connecter">Se connecter</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#" title="Créer un compte">Créer un compte</a>
         </li>
     </ul>
     <a class="paypal" href="http://www.paypal.com" title="Faire un don" target="_blank">
