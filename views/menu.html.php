4ce45a3348acf8cd4f1785823088c776e9087ef2
Valentin GALLIEN <v.gallien@yahoo.fr>
2017-10-16 14:33:06 +0200

    Amélioration du design général

--- views/menu.html.php
+++ views/menu.html.php
@@ -1,15 +1,13 @@
-<nav class="navbar navbar-expand-lg bg-light">
-  <div class="container">
+<nav class="container navbar navbar-expand-lg">
     <ul class="navbar-nav">
       <li class="nav-item">
-        <a class="nav-link" href="#" title="Se connecter">Se connecter</a>
+        <a href="#" title="Se connecter">Se connecter</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#" title="Créer un compte">Créer un compte</a>
+        <a href="#" title="Créer un compte">Créer un compte</a>
       </li>
     </ul>
     <a class="paypal" href="http://www.paypal.com" title="Faire un don" target="_blank">
       <img src="/images/paypal.png" alt="Logo Paypal" title="Paypal" />
     </a>
-  </div>
 </nav>
