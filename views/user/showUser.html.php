bdc83244376c5887e78d331825f3e3a9c8273dfc
cindy <c.cindycastel@gmail.com>
2017-10-19 16:21:19 +0200

    modifier le profil depuis mon compte

--- views/user/showUser.html.php
+++ views/user/showUser.html.php
@@ -1,14 +1,17 @@
 <h1>Mon compte</h1>
-<?= $id = $user->getId(); ?>
-<form action="/users/edit/<?= $id ; ?>" method="POST">
+<?php $id = $user->getId(); ?>
+
 <div>
-<label>Nom :  </label> <?= $user->getFirstname(); ?> </br>
-<label>Prenom :  </label> <?= $user->getLastname(); ?> </br>
-<label>Date de naissance :  </label> <?= $user->getDob(); ?> </br>
-<label>Email:  </label> <?= $user->getEmail(); ?> </br>
+	<label>Nom :  </label> <?= $user->getFirstname(); ?> </br>
+	<label>Prenom :  </label> <?= $user->getLastname(); ?> </br>
+	<label>Date de naissance :  </label> <?= $user->getDob(); ?> </br>
+	<label>Email:  </label> <?= $user->getEmail(); ?> </br>
 </div>
 
- <div class="boutonAction">
-    <input type="submit" value="Modifier" class="btn btn-default" />
-  </div>
- </form>
+
+<div >
+	<a href="/users/edit/<?= $id ?>" class="voir" >Modifier </a>
+</div>
+
+
+
