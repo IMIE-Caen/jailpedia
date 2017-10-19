29636226400c77b5b42615ab3438398cc38acd41
cindy <c.cindycastel@gmail.com>
2017-10-19 16:10:11 +0200

    vue mon compte

--- index.php
+++ index.php
@@ -54,10 +54,9 @@ $router->delete('deleteArticle','/^\/articles\/delete\/(\d+)\/?$/',
 });
 
 // users
-
-$router->get('showUser', '/^\/users\/(?<id>\d+)\/?/',
+$router->get('showUser', '/^\/users\/(\d+)\/?$/',
   function($request){
-    $id = $request->routerParams['id'];
+    $id = $request->routerParams[1];
     (new UsersController())->show($id);
 });
 $router->post('updateUser', '/^\/users\/edit\/?/',
@@ -71,7 +70,7 @@ $router->post('createUser', '/^\/users\/save\/?/',
     (new UsersController())->save($_POST);
     header("Location: /signin");
 });
-$router->get('showUser', '/^\/users\/edit\/(?<id>\d+)\/?/',
+$router->get('editUser', '/^\/users\/edit\/(?<id>\d+)\/?/',
   function($request){
     $id = $request->routerParams['id'];
     (new UsersController())->edit($id);
--- views/user/showUser.html.php
+++ views/user/showUser.html.php
@@ -1,1 +1,14 @@
 <h1>Mon compte</h1>
+<?= $id = $user->getId(); ?>
+<form action="/users/edit/<?= $id ; ?>" method="POST">
+<div>
+<label>Nom :  </label> <?= $user->getFirstname(); ?> </br>
+<label>Prenom :  </label> <?= $user->getLastname(); ?> </br>
+<label>Date de naissance :  </label> <?= $user->getDob(); ?> </br>
+<label>Email:  </label> <?= $user->getEmail(); ?> </br>
+</div>
+
+ <div class="boutonAction">
+    <input type="submit" value="Modifier" class="btn btn-default" />
+  </div>
+ </form>
