f056e9e6b16dfe15d843acd043493fb8ae5c00a3
Valentin GALLIEN <valentin@gallien.info>
2017-10-20 15:10:59 +0200

    Un chouille de style sur l'historique

--- views/article/historyArticle.html.php
+++ views/article/historyArticle.html.php
@@ -1,33 +1,20 @@
 <div class="listArticle">
-  
-
-  <?php
- 
+<?php
   if(count($articleModifs)==0) {
-
-   echo  "Aucune modification pour cet article! " ;
-
- } else {?>
- <h1>Modificication de l'article <?= $articleModifs[0]->getArticle()->getTitle() ; ?> </h1>?>
-  <?php foreach ($articleModifs as $histoArticle) { ?>
-
+    echo  "Aucune modification pour cet article! " ;
+  } else { ?>
+    <h1>Historique des modificications de l'article <?= $articleModifs[0]->getArticle()->getTitle() ; ?></h1>
+    <?php foreach ($articleModifs as $histoArticle) { ?>
     <div class="article">
-      <!-- <h2><?= $histoArticle->getArticle()->getTitle(); ?></h2>
-      <div class="article-text"><?= $histoArticle->getArticle()->getText(); ?></div> -->
-      
-       <h6>Modifié le  : <?= $histoArticle->getDateModif(); ?>  - par <?= $histoArticle->getUser()->getFirstname(); ?> </h6>
-       <h6> Titre : <?= $histoArticle->getNewTitle(); ?><h6> 
-       	<?= $histoArticle->getNewText(); ?>
-
-      </div>
-      <?php
-
-      if(isset($_SESSION['connecte']) ) { ?>
-      <div class="bottom">
-        <a href="#" class="voir" title=" Retour version ">Retourner à cette version </a>
-      </div>
-
+        <h2> Titre : <?= $histoArticle->getNewTitle(); ?></h2>
+       	<div class="description"><?= $histoArticle->getNewText(); ?></div>
+        <div>Modifié le  : <?= $histoArticle->getDateModif(); ?> par <?= $histoArticle->getUser()->getFirstname(); ?></div>
     </div>
-    
+      <?php
+        if(isset($_SESSION['connecte']) ) { ?>
+          <div class="bottom">
+            <a href="#" class="voir" title="Retour version">Retourner à cette version</a>
+          </div>
+    </div>
   <?php }}} ?>
 </div>
